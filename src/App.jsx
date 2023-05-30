70602f03c41ad4aabf64534efd6c71d7e2c46f47
ikedh <mcalderon@digitalhouse.com>
2023-05-30 11:18:15 -0300

    Template de examen, no se olviden npm i

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1>Carga de Estudiantes</h1>
+        <form></form>
+        <Card/>
+      </div>
+    </>
+  )
+}
+
+export default App
